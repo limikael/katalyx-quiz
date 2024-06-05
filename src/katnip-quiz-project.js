26ee3c2cfeba06965b7e468243bda9c373d07c2f
Mikael Lindqvist <li.mikael@gmail.com>
2024-06-05 19:52:52 +0800

    db def in plugin

---
+++ src/katnip-quiz-project.js
@@ -0,0 +1,26 @@
+import {quickminMergeConf} from "katnip-quickmin/server";
+
+let quizQuickminConf=`
+collections:
+  questions:
+    fields:
+      <Text id="question" listable fullWidth/>
+      <ReferenceMany id="alternatives" reference="alternatives" target="question_id"/>
+
+  alternatives:
+    category: hidden
+    fields:
+      <Reference id="question_id" reference="questions" disabled/>
+      <Text id="alternative" listable fullWidth/>
+
+  responses:
+    access: [admin,public]
+    fields:
+      <Text id="email" listable fullWidth/>
+      <Json id="answers" fullWidth/>
+`;
+
+export async function quickminConf(conf, ev) {
+	console.log("Register quiz db...");
+	Object.assign(conf,quickminMergeConf(quizQuickminConf,conf));	
+}
